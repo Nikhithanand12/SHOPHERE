684dbd88e6b18379ad979925807e7c9d2086762d
Nikhithanand <anand2002140@gmail.com>
2023-08-10 16:49:21 +0530

    INITIAL COMMIT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import CartList from './components/CartList';
+import { useState } from 'react';
 
 function App() {
+
+  const [product, setProduct] = useState([
+    {
+      url: 'https://rukminim1.flixcart.com/image/300/300/l51d30w0/shoe/z/w/c/10-mrj1914-10-aadi-white-black-red-original-imagft9k9hydnfjp.jpeg?q=70',
+      name: 'TRQ White Shoes',
+      category: 'Shoes',
+      seller: 'AMZ Seller Ghz',
+      price: 1999
+    },
+    {
+      url: 'https://5.imimg.com/data5/KC/PC/MY-38629861/dummy-chronograph-watch-500x500.jpg',
+      name: 'LOREM Watch Black',
+      category: 'Watches',
+      seller: 'Watch Ltd Siyana',
+      price: 2599
+    },
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq39iB_cO6uhZ59vubrqVuYTJQH-4Qa0hU9g&usqp=CAU',
+      name: 'AMZ Laptop 8GB RAM',
+      category: 'Laptops',
+      seller: 'Delhi Laptops',
+      price: 50000
+    },
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfvoDzLqrT7GwU3z7Ccp0Cl9rV0ZnU9DcmEg&usqp=CAU',
+      name: 'Security Camera',
+      category: 'CCTV',
+      seller: 'Camron LTD',
+      price: 4000
+    },
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG9e8Axt-h9q8EIybKfjGzbkIWJAr50_BX7Q&usqp=CAU',
+      name: 'Watch Pink',
+      category: 'Watches',
+      seller: 'Watch Ltd',
+      price: 2000
+    },
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9xzgtOpMxdpfgBOg3OKsEqYRkNBbuprJj4w&usqp=CAU',
+      name: 'Cup red Color',
+      category: 'Cup',
+      seller: 'ABS Ltd',
+      price: 100
+    },
+  ])
+
+  const [cart, setCart] = useState([])
+  const [showCart, setShowCart] = useState(false)
+
+  const addToCart = (data) => {
+    setCart([...cart, { ...data, quantity: 1 }])
+  }
+
+  const handleShow = (value) => {
+    setShowCart(value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header count={cart.length}
+        handleShow={handleShow} ></Header>
+
+      {
+        showCart ?
+          <CartList cart={cart} ></CartList> :
+          <ProductList product={product} addToCart={addToCart} ></ProductList>
+      }
+     
+
+
     </div>
+    
+
+    
   );
 }
 
 export default App;
+
+
